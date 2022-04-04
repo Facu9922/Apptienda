8ef741005c45cda079316e4ad8fe11966386057a
Facundo Agudo <facundo.agudo@gmail.com>
2022-04-04 17:21:30 -0300

    agregamos estilo count

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -4,11 +4,11 @@ import React from "react"
 const ItemCount =({onAdd, stock, initial, count})=>{
    
     return(
-    <div>
-        <button class="btn btn-primary" onClick={()=>{if(count < stock) onAdd ("+")}}>+</button>
+    <div class="border-bottom border-2 py-2">
+        <button class="btn btn-primary me-3" onClick={()=>{if(count < stock) onAdd ("+")}}>+</button>
              {count}
              
-         <button class="btn btn-primary" onClick={()=>{if(count > initial) onAdd ("-")}}>-</button>
+         <button class="btn btn-primary mx-3" onClick={()=>{if(count > initial) onAdd ("-")}}>-</button>
     
     </div>
     )
