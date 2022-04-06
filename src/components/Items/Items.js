2090ff520a974c9adc283f93ffbb928e45977d2b
Facundo Agudo <facundo.agudo@gmail.com>
2022-04-06 12:00:42 -0300

    agregamos cards y estilo

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -10,15 +10,26 @@ useEffect(()=>{
 getProducts().then(prods=>setProdcuts(prods)).catch(error=>console.log(error))
 },[])
 return <>
+
     {productos.map((producto)=>
-        <div class="container-fluid">
-            
-            <h2> Marca: {producto.marca} </h2>
-            <h2> Modelo: {producto.modelo} </h2>
-            <img class="w-25" src= {producto.img} alt={producto.marca} />
-            
-        </div>
+    <section class="d-flex justify-content-center">
+        <div class="card d-flex justify-content-center">
+
+            <div class="imgBox">
+                <img  src= {producto.img} alt={producto.marca} />
+            </div>
+  
+            <div class="contentBox">
+                <h3> Brand: {producto.marca}</h3>
+                <h5> Model: {producto.modelo}</h5>
+                <h2 class="price"> {producto.precio} $</h2>
+                <a href="#" class="buy">Buy Now</a>
+            </div>
+  
+    </div>
+  </section>      
         )}
 </>
 }
 export default Items
+
